6dddf138ad8fae19b1869012ee776bb931811874
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2017-11-16 15:19:20 -0500

    Added description of project to README and made changes to ShipwreckController.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# das-boot
+# Das-Boot
+
+### Das-Boot is a web application that catalogs shipwrecks across the globe.
+
--- src/main/java/com/boot/controller/ShipwreckController.java
+++ src/main/java/com/boot/controller/ShipwreckController.java
@@ -1,14 +1,20 @@
 package com.boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/")
 public class ShipwreckController {
 
     @RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
-    public List<Shipwreck> list() {
-
+    public List<String> getList() {
+        List<String> list = new ArrayList<>();
+        list.add("This is a list");
+        return list;
     }
 }
